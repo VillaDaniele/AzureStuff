02d65c381df590dbf3bf45ac8e77622165c0c5a0
VillaDaniele <101632961+VillaDaniele@users.noreply.github.com>
2024-03-20 09:48:00 +0100

    Create vmnamereservation.ps1

---
+++ VMNAMEReservation/vmnamereservation.ps1
@@ -0,0 +1,16 @@
+$CurrentAzureVMNames = Import-Csv -Path "C:\temp\azurevmnames.csv"
+$LatestAzureVMName = Import-Csv -Path "C:\temp\azurevmnames.csv" | Select-Object -Last 1
+$LatestAzureVMName = $LatestAzureVMName | foreach {$_.VMNAME}
+$NextAvailableVMName = [int16]$LatestAzureVMName.Substring($LatestAzureVMName.Length - 3)
+
+$NextVMName = $LatestAzureVMName.Substring(0,5) + ($NextAvailableVMName + 1)
+
+if ($NextVMName -in $CurrentAzureVMNames.VMNAME) {
+    Write-Host "The operation failed." -BackgroundColor DarkRed
+}
+else {
+    Write-Host "The next available name for a Virtual Machine is:" $NextVMName -BackgroundColor Gray
+    New-Object -TypeName PSCustomObject -Property @{
+    'VMNAME' = $NextVMName
+    } | Export-Csv -Path "C:\temp\azurevmnames.csv" -NoTypeInformation -Append -NoClobber
+}
