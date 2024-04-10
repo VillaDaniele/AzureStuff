38a5713a6ddbb8e69ddd765389b4123808548a80
VillaDaniele <101632961+VillaDaniele@users.noreply.github.com>
2024-04-10 14:05:53 +0200

    Update vmnamereservation.ps1

--- VMNAMEReservation/vmnamereservation.ps1
+++ VMNAMEReservation/vmnamereservation.ps1
@@ -1,3 +1,7 @@
+$SAContainerName = "azure-vmname"
+$SAName = "villatestsa"
+$SAKey = "9IqA7zObRCIcU/xB6Mj4LxBmv8Uz8itjorP/hRWgvULSvdqYP7NyvQ7Qk4/ijaEgaVOgwjEUSqLm+AStHFSbEw=="
+
 $CurrentAzureVMNames = Import-Csv -Path "/home/runner/work/AzureStuff/AzureStuff/csv/azurevmnames.csv"
 $LatestAzureVMName = Import-Csv -Path "/home/runner/work/AzureStuff/AzureStuff/csv/azurevmnames.csv" | Select-Object -Last 1
 $LatestAzureVMName = $LatestAzureVMName | foreach {$_.VMNAME}
@@ -12,5 +16,8 @@ else {
     Write-Host "The next available name for a Virtual Machine is:" $NextVMName -BackgroundColor Gray
     New-Object -TypeName PSCustomObject -Property @{
     'VMNAME' = $NextVMName
-    } | Export-Csv -Path "/home/runner/work/AzureStuff/AzureStuff/csv/azurevmnames.csv" -NoTypeInformation -Append -NoClobber
+    } | Export-Csv -Path "/home/runner/work/AzureStuff/AzureStuff/csv/azurevmnames.csv" -NoTypeInformation -Append
 }
+
+$context = New-AzStorageContext -StorageAccountName $SAName -StorageAccountKey $SAKey
+Set-AzStorageBlobContent -Force -Context $context -Container $SAContainerName -File "./azurevmnames.csv" -Blob "azurevmnames.csv"
